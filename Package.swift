06ec128c22aaf56dc2924a48535c366cc3e03e5b
Jonathan Backer <jonathan.backer@ticketmaster.com>
2025-04-03 15:15:53 -0700

    Re-added swiftSettings

--- Package.swift
+++ Package.swift
@@ -73,10 +73,13 @@ let package = Package(
         .binaryTarget(
             name: "TicketmasterTickets",
             path: "Frameworks/TicketmasterTickets.xcframework",
-            swiftSettings: [
-                .swiftLanguageMode(.v5)
-            ]
-        )
+        ),
+        swiftSettings: [
+            .swiftLanguageMode(.v5)
+        ]
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageVersions: [.v5],
+    swiftSettings: [
+        .swiftLanguageMode(.v5)
+    ]
 )
